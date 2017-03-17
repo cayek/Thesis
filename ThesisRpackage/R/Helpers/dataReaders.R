606beff931750a13163a4fc1380d741a898cdeef
cayek <kevin.caye@gmail.com>
2017-03-17 16:31:50 +0100

    Fin de journee

--- ThesisRpackage/R/Helpers/dataReaders.R
+++ ThesisRpackage/R/Helpers/dataReaders.R
@@ -37,3 +37,44 @@ read_X <- function(X.file) {
     stop("TODO")
   }
 }
+
+#' read vcf files
+#'
+#' Rmk: it uses LEA::vcf2geno
+#'
+#' @param file.pattern
+#' @param keep.indiv keep only this indiv
+#' @param maf.threshold filter with this maf threshold
+#' @param subsample.rate subsample with this rate
+#'
+#' @export
+read_vcf <- function(file.pattern, maf.threshold = NULL, subsample.rate = NULL) {
+  ## assert
+  TestRequiredPkg("LEA")
+
+  files <- list.files()
+  files <- grep(file.pattern, files, value = TRUE)
+
+  res <- list()
+  res$G <- NULL
+  res$snps.info <- tibble()
+  for (f in files) {
+    output.tmp <- tempfile(tmpdir = ".", fileext = ".geno")
+    LEA::vcf2geno(f, output.file = output.tmp, force = TRUE)
+    geno <- LEA::read.geno(output.tmp)
+    snps.info <- readr::read_delim(sub(pattern = "\\.geno$", "\\.vcfsnp", output.tmp),
+                                   delim = " ",
+                                   col_names = FALSE,
+                                   progress = FALSE)
+    colnames(geno) <- snps.info$X3
+
+    ## filter maf
+    if (!is.null(maf.threshold)) {
+      maf <- apply(geno, 2, function(locus) {p <- mean(locus); min(p, 1 - p)})
+      cat("== Removing", mean(maf < maf.threshold),"% loci")
+      geno <- geno[,maf >= maf.threshold]
+    }
+    res$G <- cbind(geno, res$G)
+  }
+}
+
