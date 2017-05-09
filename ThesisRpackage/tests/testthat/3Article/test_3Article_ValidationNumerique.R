6a880c113da0f7f4722d78820b576681655efc1e
cayek <kevin.caye@gmail.com>
2017-05-09 16:08:18 +0200

    deploy on krakenator

--- ThesisRpackage/tests/testthat/3Article/test_3Article_ValidationNumerique.R
+++ ThesisRpackage/tests/testthat/3Article/test_3Article_ValidationNumerique.R
@@ -55,7 +55,8 @@ test_that("run of main exp", {
 test_that("Play with experiment", {
 
   skip("run and play")
-  
+
+  library(ThesisRpackage)
   G.file <- "~/Projects/Thesis/Data/ThesisDataset/3Article/1000GenomesPhase3/ValidationNumerique_EU_L5e+05.G.rds"
 
   ## methods
@@ -71,7 +72,7 @@ test_that("Play with experiment", {
                                    K.method = 2,
                                    cs = 0.4,
                                    cs.sum = TRUE,
-                                   sd.V.rho = 2, 
+                                   sd.V.rho = 1, 
                                    nb.rep = 4,
                                    fast.only = TRUE,
                                    cluster.nb = 4,
