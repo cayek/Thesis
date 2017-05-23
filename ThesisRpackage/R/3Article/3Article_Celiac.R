6ca1847c3d8ac1bfb230996429b93d1314781c66
cayek <kevin.caye@gmail.com>
2017-05-23 14:05:05 +0200

    deploy on krakenator

--- ThesisRpackage/R/3Article/3Article_Celiac.R
+++ ThesisRpackage/R/3Article/3Article_Celiac.R
@@ -4,13 +4,13 @@ Article3_Celiac_sampler <- function(clumped = TRUE) {
   if (clumped) {
     s <- TrueSampler(G.file = "~/Projects/Thesis/Data/ThesisDataset/3Article/Celiac/G_clumped.rds",
                      X.file = "~/Projects/Thesis/Data/ThesisDataset/3Article/Celiac/X.rds",
-                     outlier.file = NULL,
+                     outlier.file = "~/Projects/Thesis/Data/ThesisDataset/3Article/Celiac/gwas_catalog_candidates_clumped.rds",
                      n = NULL,
                      L = NULL)
   } else {
     s <- TrueSampler(G.file = "~/Projects/Thesis/Data/ThesisDataset/3Article/Celiac/G.rds",
                      X.file = "~/Projects/Thesis/Data/ThesisDataset/3Article/Celiac/X.rds",
-                     outlier.file = NULL,
+                     outlier.file = "~/Projects/Thesis/Data/ThesisDataset/3Article/Celiac/gwas_catalog_candidates.rds",
                      ind.clumping = "~/Projects/Thesis/Data/ThesisDataset/3Article/Celiac/ind.clumpling.rds",
                      n = NULL,
                      L = NULL)
